10ff69142c4210606b3d51af07e33fc0e7c7e8e3
JonatanV <nofettatjarn1>
2019-11-19 10:11:07 +0100

    lade till dokumentation

--- src/Clock.java
+++ src/Clock.java
@@ -5,29 +5,63 @@ public class Clock {
     int minLimit = 60;
     int houLimit = 24;
 
+    /**
+     * Sets hours and minutes to their respective limits.
+     */
+
     public Clock() {
         this.hours = new NumberDisplay(houLimit);
         this.minutes = new NumberDisplay(minLimit);
     }
+
+    /**
+     * Sets a value to hours and minutes.
+     * @param hours sets the value to 0
+     * @param minutes sets the value to 0
+     */
+
     public Clock(int hours, int minutes){
         this();
         this.hours.setValue(0);
         this.minutes.setValue(0);
     }
+
+    /**
+     * Increases minutes and if it becomes 0 hours increases.
+     */
+
     public void timeTick(){
         minutes.increment();
         if (minutes.getValue() == 0){
             hours.increment();
         }
     }
+
+    /**
+     *  Sets the time using hours and minutes
+     * @param hours sets the value of hours
+     * @param minutes  sets the value of hours
+     */
+
     public void setTime(int hours, int minutes){
         this.hours.setValue(hours);
         this.minutes.setValue(minutes);
     }
+
+    /**
+     * Gets the time by returning displayString
+     * @return returns displayString
+     */
+
     public String getTime(){
 
         return displayString;
     }
+
+    /**
+     * updates the display
+     */
+
     private void updateDisplay(){
         displayString = hours.getDisplayValue() +':'+ minutes.getDisplayValue();
     }
--- src/NumberDisplay.java
+++ src/NumberDisplay.java
@@ -2,20 +2,41 @@ public class NumberDisplay {
     private int limit;
     private int value;
 
+    /**
+     * Creates a NuberDisplay with a starting value of 0 and a maxLimit
+     * @param maxLimit limits NumberDisplay max value
+     */
+
     public NumberDisplay(int maxLimit) {
         value = 0;
         limit = maxLimit;
     }
 
+    /**
+     * gets the value of value
+     * @return returns the value oc the variable value
+     */
+
     public int getValue() {
         return value;
     }
+
+    /**
+     * Sets a value to the NumberDisplay and if its greater than limit and less than zero nothing happens. Else it sets value to new value
+     * @param newValue
+     */
 
     public void setValue(int newValue) {
         if(newValue >limit && newValue < 0){
 
         }else {value = newValue;}
     }
+
+    /**
+     * Makes DisplayValue into a string. If value is less than ten it puts a zero before DisplayValue.
+     * @return REeturns DisplayValue.
+     */
+
     public String getDisplayValue(){
         String displayValue = Integer.toString(value);
         if (value < 10){
@@ -23,6 +44,11 @@ public class NumberDisplay {
         }
         return displayValue;
     }
+
+    /**
+     * Value increases until it hits limit when it does it turns value into zero.
+     */
+
     public void increment(){
         value++;
         if(value==limit){
